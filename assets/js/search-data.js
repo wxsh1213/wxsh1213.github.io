8bc6a6078bfb4b86ca8e436e67893d4200ea06c5
wxsh1213 <90723984+wxsh1213@users.noreply.github.com>
2025-02-15 21:42:24 +0000

    Deploying to gh-pages from @ wxsh1213/wxsh1213.github.io@524d45c49a7f0637b1eb6024dc8d349460c9fe54 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,18 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+        },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
